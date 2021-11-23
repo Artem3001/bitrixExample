8204d26999a1afa0bde60a9c2cdaf23e48997f42
Артем <88019398+Artem3001@users.noreply.github.com>
2021-11-23 17:23:40 +0500

    Add files via upload

---
+++ bottom.menu.php
@@ -0,0 +1,64 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Каталог",
+		"#",
+		Array(),
+        Array(
+            'CLASS' => 'footer-menu__item',
+            'A_CLASS' => 'footer-menu__link'
+        ),
+        ""
+	),
+
+	Array(
+		"Контакты",
+		"#",
+		Array(),
+        Array(
+            'CLASS' => 'footer-menu__item',
+            'A_CLASS' => 'footer-menu__link'
+        ),
+		"" 
+	),
+
+	Array(
+		"Как стать партнером?",
+		"#",
+		Array(),
+        Array(
+            'CLASS' => 'footer-menu__item',
+            'A_CLASS' => 'footer-menu__link'
+        ),
+        ""
+	),
+	Array(
+		"Вопрос/Ответ",
+		"faq/",
+		Array(),
+        Array(
+            'CLASS' => 'footer-menu__item',
+            'A_CLASS' => 'footer-menu__link'
+        ),
+        ""
+	),
+	Array(
+		"Статьи",
+		"#",
+		Array(),
+        Array(
+            'CLASS' => 'footer-menu__item',
+            'A_CLASS' => 'footer-menu__link'
+        ), 		""
+	),
+	Array(
+		"Новости",
+		"#",
+		Array(),
+        Array(
+            'CLASS' => 'footer-menu__item',
+            'A_CLASS' => 'footer-menu__link'
+        ), 		""
+	),
+);
+?>
---
+++ top.menu.php
@@ -0,0 +1,74 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$aMenuLinks = Array(
+	Array(
+		"Каталог",
+		"#",
+		Array(), 
+		Array(
+		    'CLASS' => 'main-nav__item',
+		    'A_CLASS' => 'main-nav__link',
+        ),
+		"" 
+	),Array(
+		"Про LEINOS",
+		"#",
+		Array(),
+		Array(
+		    'CLASS' => 'main-nav__item',
+            'A_CLASS' => 'main-nav__link',
+
+        ),
+		""
+	),Array(
+		"Цвета",
+		"#",
+		Array(),
+		Array(
+		    'CLASS' => 'main-nav__item',
+		    'A_CLASS' => 'main-nav__link',
+
+        ),
+		""
+	),Array(
+		"Портфолио",
+		"portfolio/",
+		Array(),
+		Array(
+		    'CLASS' => 'main-nav__item',
+		    'A_CLASS' => 'main-nav__link',
+
+        ),
+		""
+	),Array(
+		"Сертификаты",
+		"#",
+		Array(),
+		Array(
+		    'CLASS' => 'main-nav__item',
+		    'A_CLASS' => 'main-nav__link',
+
+        ),
+		""
+	),Array(
+		"Вопросы и ответы",
+		"faq/",
+		Array(),
+		Array(
+		    'CLASS' => 'main-nav__item',
+		    'A_CLASS' => 'main-nav__link',
+
+        ),
+		""
+	),Array(
+		"Где купить",
+		"dealers/",
+		Array(),
+		Array(
+		    'CLASS' => 'main-nav__item',
+		    'A_CLASS' => 'main-nav__link',
+
+        ),
+		""
+	),
+);
+?>
